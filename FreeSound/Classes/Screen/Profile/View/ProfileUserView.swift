775f920ce9d5abe73e2941a895ecd91d30cbd762
Anton Shcherba <anton.shcherba@ormlondon.com>
2018-03-15 14:02:45 +0200

    Added ProfileUserView

---
+++ FreeSound/Classes/Screen/Profile/View/ProfileUserView.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileUserView.swift
+//  FreeSound
+//
+//  Created by Anton Shcherba on 3/15/18.
+//  Copyright © 2018 Anton Shcherba. All rights reserved.
+//
+
+import UIKit
+
+class ProfileUserView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func setup() {
+        
+    }
+}
